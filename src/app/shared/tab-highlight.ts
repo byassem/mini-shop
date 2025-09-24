d9935e70cf85c47f100b02d5bdbe4d6d3b09ee17
assem diabagate <assemdgt@gmail.com>
2025-09-24 09:16:26 +0000

    Setup & Configuration project

---
+++ src/app/shared/tab-highlight.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appTabHighlight]'
+})
+export class TabHighlight {
+
+  constructor() { }
+
+}
---
+++ src/app/shared/tabs-tab/tabs-tab.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs-tab',
+  imports: [],
+  templateUrl: './tabs-tab.html',
+  styleUrl: './tabs-tab.scss'
+})
+export class TabsTab {
+
+}
